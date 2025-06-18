93a8938f571231ed83f3ca3366b8ff59108d4192
Kyle <82291919+kbprophecy21@users.noreply.github.com>
2025-06-18 04:21:43 +0000

    finish homework

--- Chapter18_19/Lab_Assignment_1/Problem2.java
+++ Chapter18_19/Lab_Assignment_1/Problem2.java
@@ -1,39 +1,41 @@
-import java.util.Scanner;
+
 
 public class Problem2 {
 
-    // Recursive method to calculate edit distance
-    public static int editDistance(String str1, String str2) {
-        // Base Cases
-        if (str1.length() == 0) {
-            return str2.length(); // need to insert all of str2
+    
+    public static int editDistance(String word_Number_1, String word_Number_2) {
+
+       
+        if (word_Number_1.length() == 0) {
+            return word_Number_2.length(); 
         }
-        if (str2.length() == 0) {
-            return str1.length(); // need to delete all of str1
+        if (word_Number_2.length() == 0) {
+            return word_Number_1.length(); 
         }
 
-        // If first characters match, skip them
-        if (str1.charAt(0) == str2.charAt(0)) {
-            return editDistance(str1.substring(1), str2.substring(1));
+        
+        if (word_Number_1.charAt(0) == word_Number_2.charAt(0)) {
+            return editDistance(word_Number_1.substring(1), word_Number_2.substring(1));
         }
 
         // Otherwise try insert, delete, and replace
-        int insertOp = 1 + editDistance(str1, str2.substring(1));
-        int deleteOp = 1 + editDistance(str1.substring(1), str2);
-        int replaceOp = 1 + editDistance(str1.substring(1), str2.substring(1));
+        int NumberOfInserts = 1 + editDistance(word_Number_1, word_Number_2.substring(1));
+        int NumberOfDeletes = 1 + editDistance(word_Number_1.substring(1), word_Number_2);
+        int NumberOfReplacement = 1 + editDistance(word_Number_1.substring(1), word_Number_2.substring(1));
 
         // Return the minimum of the three operations
-        return Math.min(insertOp, Math.min(deleteOp, replaceOp));
+        return Math.min(NumberOfInserts, Math.min(NumberOfDeletes, NumberOfReplacement));
     }
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter first string: ");
-        String first = scanner.nextLine();
-        System.out.print("Enter second string: ");
-        String second = scanner.nextLine();
+       
+        int amountOfChanges = editDistance("kitten", "sitting");
+        System.out.println("Number of changes from kitten to sitting: " + amountOfChanges);
 
-        int distance = editDistance(first, second);
-        System.out.println("Edit distance: " + distance);
+        amountOfChanges = editDistance("flaw", "lawn");
+        System.out.println("Number of changes from for flaw to lawn: " + amountOfChanges);
+
+        amountOfChanges = editDistance("intention", "execution");
+        System.out.println("Number of changes from for intention to execution: " + amountOfChanges);
     }
 }
--- Chapter18_19/Lab_Assignment_1/Problem3.java
+++ Chapter18_19/Lab_Assignment_1/Problem3.java
@@ -5,15 +5,13 @@ import java.util.*;
 
 public class Problem3 {
 
-
-
     public static <T extends Comparable<T>> T findMax(List<ComparableContainer<T>> containers)
     {
 
-        T max = containers.get(0).getGreater();
+        T max = containers.get(0).getGreaterThan();
 
         for (ComparableContainer<T> container : containers) {
-            T current = container.getGreater();
+            T current = container.getGreaterThan();
             if (current.compareTo(max) > 0) {
                 max = current;
             }
@@ -22,44 +20,48 @@ public class Problem3 {
         return max;
     }
 
-
-
-
-public class ComparableContainer<T extends Comparable<T>> {
-    private T first;
-    private T second;
-
-    public ComparableContainer(T first, T second) {
-        this.first = first;
-        this.second = second;
-    }
-
-    public T getGreater() {
-        return (first.compareTo(second) >= 0) ? first : second;
-    }
-}
-
-
-
     public static void main(String[] args) {
 
-        
-        // Integer example
         List<ComparableContainer<Integer>> intContainers = new ArrayList<>();
+
         intContainers.add(new ComparableContainer<>(3, 8));
         intContainers.add(new ComparableContainer<>(5, 2));
         intContainers.add(new ComparableContainer<>(10, 7));
 
-        Integer maxInt = findMax(intContainers);
-        System.out.println("Max Integer: " + maxInt);
+        Integer maxNumber = findMax(intContainers);
+        System.out.println("The Max Integer Value: " + maxNumber);
 
-        // String example
+        
         List<ComparableContainer<String>> stringContainers = new ArrayList<>();
-        stringContainers.add(new ComparableContainer<>("apple", "banana"));
-        stringContainers.add(new ComparableContainer<>("dog", "cat"));
-        stringContainers.add(new ComparableContainer<>("zebra", "lion"));
+        stringContainers.add(new ComparableContainer<>("Pineapples", "Avaccados"));
+        stringContainers.add(new ComparableContainer<>("Lizard", "Snake"));
+        stringContainers.add(new ComparableContainer<>("Blueberry", "Orange"));
 
-        String maxStr = findMax(stringContainers);
-        System.out.println("Max String: " + maxStr);
+        String maxWord = findMax(stringContainers);
+        System.out.println("The max value string is: " + maxWord);
+    }
+
+
+    public static class ComparableContainer <T extends Comparable<T>> {
+        public T value_1;
+        public T value_2;
+
+        public ComparableContainer(T value_1, T value_2) {
+            this.value_1 = value_1;
+            this.value_2 = value_2;
+        }
+
+        public T getGreaterThan() {
+            if (value_1.compareTo(value_2) >= 0) {
+                return value_1;
+            } else {
+                return value_2;
+            }
+        }
     }
 }
+
+
+
+  
+
